6560ffa605c4d1da4c5de6520a01af12bfe2a69e
Brian Hicks <brian@brianthicks.com>
2016-04-25 16:26:48 -0500

    docker: improve pretty much everything

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,23 @@ FROM centos:7
 
 MAINTAINER Brian Hicks <brian@aster.is>
 
-# add development tools
+# development tools
 RUN yum groupinstall -y "Development Tools"
 
-# add FPM
+# FPM
 RUN yum install -y ruby ruby-devel
 RUN gem install fpm --no-rdoc --no-ri
 
-# TODO: add scripts
+# peer packages
+COPY mantl-rpm.repo /etc/yum.repos.d/
+
+# hammer
+ADD https://github.com/asteris-llc/hammer/releases/download/1.0.0/hammer_1.0.0_linux_amd64.tar.gz /tmp/hammer.tgz
+RUN cd /tmp && \
+    tar -xzvf /tmp/hammer.tgz && \
+    cp /tmp/hammer_1.0.0_linux_amd64/hammer /usr/bin/hammer && \
+    rm -rf /tmp/hammer*
+
+# scripts
+COPY entry.sh /bin/entry
+ENTRYPOINT ["/bin/entry"]
---
+++ entry.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# make sure we have the src directory
+if [ ! -d "/src" ]; then
+    echo "Please add source as a volume at /src"
+fi
+
+cd /src
+
+command=${1:-}
+case "$command" in
+    build)
+        hammer build
+        ;;
+    test)
+        section=${2:-}
+        name=${3:-}
+        case "$section" in
+            install)
+                yum install -y out/${name}*.rpm
+                yum remove -y $name
+                ;;
+            *)
+                echo "usage: $0 test SECTION"
+                echo 'try "install" or "upgrade"'
+                exit 1
+                ;;
+        esac
+        ;;
+    *)
+        echo "usage: $0 COMMAND"
+        echo 'try "build" or "test"'
+        exit 1
+        ;;
+esac
